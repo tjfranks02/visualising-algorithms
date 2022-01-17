bef0fe5ddb8aa348b67995f766ec978044efac35
Tyson Franks <tjfranks02@hotmail.com>
2022-01-17 14:03:25 +1000

    Refactor of main.py to MVC structure. Added classes BSTController and BSTView to communicate with underlying BST data structure.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,83 @@
-from tkinter import *
-from tkinter import ttk
-import bst as bst
+import PySimpleGUI as sg
 
-root = Tk()
+GRAPH_DIMENSIONS = (500, 500)
+GRAPH_DIMENSION = 500
 
-canvas = Canvas(root, width=500, height=400, background='gray75')
-canvas.create_oval(10, 10, 50, 50, fill='red', outline='blue')
-canvas.grid(column=0, row=0, sticky=(N, W, E, S))
 
-canvas.create_line(10, 10, 200, 50, 90, 150, 50, 80, smooth="1")
+class BSTController:
+    """
+    coordinating class enabling communication between view and model for BST.
+    """
 
-root.mainloop()
+    def __init__(self, window):
+        """
+        initialise a controller that aids in displaying a BST
+
+        parameters:
+            window (PSG::Window): the window in which to draw the BST
+        """
+        self.window = window
+
+    def main_loop(self):
+        
+        while True:
+            
+            #await events on the window
+            event, values = self.window.read()
+            print(event)
+            print(values)
+
+            if event in (sg.WIN_CLOSED, 'Cancel'):
+                break
+
+
+class BSTView:
+    """
+    this is a class capable of displaying a binary search tree upon a PSG graph
+    element and performing various animations to display the process of
+    performing various actions on the tree.
+    """
+
+    def __init__(self, graph):
+        """
+        initialise the view of the BST
+
+        parameters:
+            graph (PSG::Graph): the graph element to display the tree on
+        """
+        self.graph = graph
+
+
+
+
+
+"""
+INITIALISATION CODE FOR WINDOW AND GUI
+"""
+sg.theme('DarkAmber')   # Add a little color to your windows
+# All the stuff inside your window. This is the PSG magic code compactor...
+
+input_layout = [
+    [sg.Text("Binary search tree")],
+    [sg.OptionMenu(values=("Insert", "Delete", "Search"), default_value="Insert", key="BST_METHOD")],
+    [sg.Input(key="BST_ACTION_VAL", enable_events=True), sg.Button("Perform action", enable_events=True, key="BST_TREE_ACTION")]
+]
+
+graphing_layout = [
+    sg.Graph(
+        GRAPH_DIMENSIONS, (0,0), GRAPH_DIMENSIONS,
+        background_color="red", 
+        key='-GRAPH-', 
+        enable_events=True, 
+    )
+]
+
+layout = input_layout + [graphing_layout]
+
+# Create the Window
+window = sg.Window('Window Title', layout, finalize=True)
+
+controller = BSTController(window)
+controller.main_loop()
+
+window.close()
