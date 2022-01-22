1d60a159ae37b07da27b372d08d7069efa9c7608
Tyson Franks <tjfranks02@hotmail.com>
2022-01-22 19:36:40 +1000

    Added button so that users can step through search tree operations frame by frame. Also added function to redraw nodes to remove the animation effects placed on them

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+import time
 
 import bst
 
@@ -18,6 +19,7 @@ BST_TREE_ACTION = "BST_TREE_ACTION"
 BST_GRAPH = "BST_TREE_CANVAS"
 BST_METHOD = "BST_METHOD"
 BST_ACTION_VAL = "BST_ACTION_VAL"
+BST_FORWARD = "BST_FORWARD"
 
 """
 methods on bst tree
@@ -80,43 +82,54 @@ class BSTController:
         self.tree_model = None
 
     def main_loop(self):
-        counter = 0
+        """
+        the main loop processing input from window and displaying tree.
+        """
+        instruction_queue = []
+        prev_instruction = None
+        animating = False
+        tree_height = -1
+
         while True:
-            
             #await events on the window
             event, values = self.window.read()
-            
+
             if event == sg.WIN_CLOSED:
                 break
+            
+            #user requests next step in animation
+            if event == BST_FORWARD and animating:
 
+                if len(instruction_queue) == 0:
+                    animating = False
+                    self.view.redraw_nodes()
+                    continue
+
+                self.view.animate_path(prev_instruction, instruction_queue[0], 
+                    tree_height, 0)
+                prev_instruction = instruction_queue.pop(0)
+
+            #a tree method has been requested
             if event == BST_TREE_ACTION:
-                method = values[BST_METHOD]
+                
                 value = values[BST_ACTION_VAL]
+                method = values[BST_METHOD]
 
                 if method == BST_INSERT:
                     self.tree_model, path, height, level = bst.insert(
                         self.tree_model, int(value))
-                    self.view.animate_path(path, height, level)
-                elif method == BST_DELETE:
-                    self.delete_node(value)
-                else:
-                    self.search_tree(value)
+                    instruction_queue = path
+                    print(instruction_queue)
+                    animating = True
                     
-    def insert_node(self, value):
+            
 
-        if self.tree_model == None:
-            self.tree_model = bst.create(value)
-        else:
-            self.tree_model = bst.insert(self.tree_model, value)
-    
-    def delete_node(self, value):
-        if self.tree_model != None:
-            self.tree_model = bst.delete(self.tree_model, value)
-
-    def search_tree(self, value):
-        
-        if self.tree_model != None:
-            node = bst.search(self.tree_model, value)
+          
+    def remove_items(self, items):
+        """
+        """
+        for item in items:
+            self.graph.delete_figure(item)
 
 
 
@@ -135,6 +148,12 @@ class BSTNode:
         self.x_coord = x_coord
         self.y_coord = y_coord
 
+    def get_coords(self):
+        return (self.x_coord, self.y_coord)
+    
+    def set_coords(self, coords):
+        self.x_coord = coords[0]
+        self.y_coord = coords[1]
 
 class BSTView:
     """
@@ -152,8 +171,32 @@ class BSTView:
         """
         self.graph = graph
         self.tree_vals = {}
+        self.redraw = [] #list of nodes queued to redraw
 
-    def animate_path(self, path, height, level):
+    def redraw_nodes(self):
+        """
+        redraws nodes that have been highlighted in the default colour.
+        """
+        for node_value in self.redraw:
+            node = self.tree_vals[node_value]
+            node_id = node.node_id
+            text_id = node.text_id
+            
+            #delete the old figures
+            self.graph.delete_figure(node_id)
+            self.graph.delete_figure(text_id)
+
+            #redraw figures with default colour
+            coords = node.get_coords()
+            node_id = self.graph.draw_circle(coords,
+                fill_color="yellow", radius=20)
+            
+            text_id = self.graph.draw_text(node_value, coords)
+
+            self.redraw = []
+        
+
+    def animate_path(self, previous, current, height, level):
         """
         takes a path returned from a bst and animate the process, displaying it
         on the graph.
@@ -169,33 +212,35 @@ class BSTView:
         """
         prev_instruction = None
 
-        for index in range(0, len(path)):
+        if previous != None:
+            prev_instruction = previous[0]
 
-            instruction = path[index]
+        instruction = current[0]
 
-            if instruction == FIND:
-                pass
-            elif instruction == SEARCH:
-                pass
-            elif instruction == SWAP:
-                pass
-            elif instruction[0] == INSERT:
-                self.animate_insert(prev_instruction, instruction, 
-                    height, level)
-            elif instruction == NOT_FOUND:
-                pass
-            elif instruction == DUPLICATE:
-                pass
-            elif instruction == DELETE:
-                pass
-            elif instruction == RESTRUCTURE:
-                pass
-
-            prev_instruction = instruction
+        if instruction == FIND:
+            pass
+        elif instruction == SEARCH:
+            pass
+        elif instruction == SWAP:
+            pass
+        elif instruction == INSERT:
+            self.animate_insert(previous, current, height, level)
+        elif instruction == NOT_FOUND:
+            pass
+        elif instruction == DUPLICATE:
+            pass
+        elif instruction == DELETE:
+            pass
+        elif instruction == RESTRUCTURE:
+            pass
+        
+        prev_instruction = instruction
 
 
     def animate_insert(self, prev_instruction, new_instruction, height, level):
         """
+        function to display an animation of displaying an insertion of a node.
+
         parameters:
             prev_instruction ((string, int)):
             new_instruction ((string, int)):
@@ -205,32 +250,40 @@ class BSTView:
         prev_val = None
         new_val = new_instruction[1]
 
+        print("previous instruction:", prev_instruction)
+        print("current instruction:", new_instruction)
+
         #the tree is not empty
         if prev_instruction != None:
             prev_val = prev_instruction[1]
             prev_node = self.tree_vals[prev_val]
 
-            new_x = prev_node.x_coord - 50
-            new_y = prev_node.y_coord - 50
+            new_x = 0
+            new_y = 0
 
-            print(new_x, new_y)
-
-            circle_id = self.graph.draw_circle((new_x, new_y), 
-                fill_color='yellow', radius=20)
-            text_id = self.graph.draw_text(new_val, (new_x, new_y))
-            self.tree_vals[new_val] = BSTNode(circle_id, text_id, x_coord=new_x, 
-                y_coord=new_y)
-
+            if prev_val > new_val:
+                new_x = prev_node.x_coord - 50
+                new_y = prev_node.y_coord - 50
+            else:
+                new_x = prev_node.x_coord + 50
+                new_y = prev_node.y_coord - 50
+        
+        #tree empty, add root element
         else:
-            circle_id = self.graph.draw_circle(ROOT_COORDS, 
-                fill_color='yellow', radius=20)
-            text_id = self.graph.draw_text(new_val, ROOT_COORDS)
-            self.tree_vals[new_val] = BSTNode(circle_id, text_id, x_coord=250, 
-                y_coord=450)
+            new_x = ROOT_COORDS[0]
+            new_y = ROOT_COORDS[1]
 
+        #draw necessary shapes on graph
+        node_id = self.graph.draw_circle((new_x, new_y),
+            fill_color="green", radius=20)
+        text_id = self.graph.draw_text(new_val, (new_x, new_y))
+        self.tree_vals[new_val] = BSTNode(node_id, text_id, x_coord=new_x, 
+                y_coord=new_y)
+        self.redraw.append(new_val)
 
- 
+        print(new_x, new_y)
 
+        
 """
 INITIALISATION CODE FOR WINDOW AND GUI
 """
@@ -240,7 +293,8 @@ sg.theme('DarkAmber')   # Add a little color to your windows
 input_layout = [
     [sg.Text("Binary search tree")],
     [sg.OptionMenu(values=(BST_INSERT, BST_DELETE, BST_SEARCH), default_value=BST_INSERT, key=BST_METHOD)],
-    [sg.Input(key=BST_ACTION_VAL, enable_events=True), sg.Button("Perform action", enable_events=True, key=BST_TREE_ACTION)]
+    [sg.Input(key=BST_ACTION_VAL, enable_events=True), sg.Button("Perform action", enable_events=True, key=BST_TREE_ACTION)],
+    [sg.Button("Next", key=BST_FORWARD, enable_events=True)]
 ]
 
 graphing_layout = [
